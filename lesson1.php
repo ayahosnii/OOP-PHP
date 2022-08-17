a68b4f86425111d4851e7d417c8384c853284c45
Aya Hosny <ayazaine@gmail.com>
2022-08-17 21:32:10 +0200

    lesson1

--- lesson1.php
+++ lesson1.php
@@ -1,4 +1,8 @@
 <?php
+// Class represent a template consists of properties (attribute) and method (functions)
+// A Object is an instance of typical class
+// By default the attribute value is NULL
+
 class Employee // template contain Properties and operators
 {
     public $name; //=> $name is Property || public is defining the Property
@@ -11,23 +15,58 @@ class Employee // template contain Properties and operators
     public $overtimeRate;
     public $abbsent;
     public $abbsentRate;
+
+    function calculateOverTime()
+    {
+        return $this->overtime * $this->overtimeRate;
+    }
+
+    function calculateAbbsent()
+    {
+        return $this->abbsent * $this->abbsentRate;
+    }
+    function calculateTotalSalary()
+    {
+        return ($this->salary - ($this->salary * $this->tax) + $this->calculateOverTime() - $this->calculateAbbsent());
+    }
+
 }
 
-$ahmed = new Employee(); // =>ahmed is an objech has properties;
-$ahmed->name = 'Aya';
-$ahmed->age = 24;
-$ahmed->address = 'St.';
-$ahmed->salary = 20000;
-$ahmed->tax = 0.03;
-$ahmed->overtime = 20;
-$ahmed->overtimeRate = 15;
-$ahmed->abbsent = 1;
-$ahmed->abbsentRate = 75;
-//echo $ahmed-> name;
-//is_a($ahmed, 'Employee');
-echo '<pre>';
-var_dump($ahmed);
-echo '<pre>';
+$aya = new Employee(); // =>ahmed is an objech has properties;
+$aya->name = 'Aya';
+$aya->age = 24;
+$aya->address = 'St.';
+$aya->salary = 20000;
+$aya->tax = 0.03;
+$aya->overtime = 20;
+$aya->overtimeRate = 15;
+$aya->abbsent = 1;
+$aya->abbsentRate = 75;
+
+
+$mohammed = new Employee(); // =>ahmed is an objech has properties;
+$mohammed->name = 'Mohammed';
+$mohammed->age = 25;
+$mohammed->address = 'Alex';
+$mohammed->salary = 19000;
+$mohammed->tax = 0.02;
+$mohammed->overtime = 40;
+$mohammed->overtimeRate = 10;
+$mohammed->abbsent = 0;
+$mohammed->abbsentRate = 50;
+
+
+echo $aya-> name . ' has overtime = ' .$aya->calculateOverTime();
+echo ' <pre>';
+echo $aya-> name . ' has abbsent = ' .$aya->calculateAbbsent();
+echo ' <pre>';
+echo $mohammed-> name . ' has overtime of ' .$mohammed->calculateOverTime();
+//is_a($aya, 'Employee');
+//echo $aya-> name;
+//is_a($aya, 'Employee');
+//echo '<pre>';
+//var_dump($aya);
+//echo '<pre>';
 
 class Car
 {
@@ -44,9 +83,11 @@ $bmw->speed = 320;
 $bmw->doors = 2;
 $bmw->rims = 'Sports';*/
 
-function calculateOverTime($overtime, $overtimeRate)
+
+/*function calculateOverTime($overtime, $overtimeRate)
 {
     return $overtime * $overtimeRate;
 }
 
-$ahmedOvertimeCalculation = calculateOverTime($ahmed->overtime, $ahmed->overtimeRate);
+$ayaOvertimeCalculation = calculateOverTime($aya->overtime, $aya->overtimeRate);
+echo 'ahmed = '. $ahmedOvertimeCalculation;*/

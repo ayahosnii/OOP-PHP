1fae52359928261e8b04836c6410f934b44c9815
Aya Hosny <ayazaine@gmail.com>
2022-08-19 03:16:11 +0200

    lesson3 [constructor and  Access Modifiers].php

---
+++ lesson3 [constructor and  Access Modifiers].php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * Constructor  & Destructor
+ * Access Modifiers
+ * Inheritance
+ *
+ * */
+
+//Class Context
+/*
+ * Access Modifier
+ * access modifier like $ahmed-> name [access of name]
+ * Public ان الخاصية او الدالة يمكن استدعاؤها داخل سياق عمل الكلاس او حارج الكلاس مياشرة من اي كائن من هذا النوع
+ * Private ان الخاصية او الدالة لا يمكن اطلاقا استدعاؤها حارج ميثاق عمل الكلاس
+ * Protected ان الخاصية او الدالة لا يمكن  استدعاؤها حارج الكلاس ولكن يمكن  استدعاؤها داخل اي الكلاس ابن لهذا النوع
+ * */
+// setters and getters
+class Student
+{
+    private $name;
+    private $age;
+    private $level;
+    private $score;
+    private $subjects = array(
+        'Arabic' => 0,
+        'English' => 0,
+        'Math' => 0,
+        'Science' => 0,
+    );
+
+    protected $minScore = 150;
+    public $maxScore = 300;
+
+    public function __construct($name, $age) //=>magic function
+    {
+        $this->setName($name);
+        $this->setAge($age);
+    }
+
+    public function setName($name)
+    {
+        $name = strtolower($name);
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+        $name = ucwords($name);
+        $name = substr($name, 0, 20);
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    private function setAge($age)
+    {
+        $age = filter_var($age, FILTER_SANITIZE_NUMBER_INT);
+        $age = abs($age);
+        if ($age < 20 || $age > 30){
+            throw new Exception('Sorry age must be between 12 and 20');
+        }else{
+            $this->age = $age;
+        }
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function getSubject()
+    {
+        return $this->subjects;
+    }
+    public function getMinScore()
+    {
+        return $this->minScore;
+    }
+    public function getMaxScore()
+    {
+        return $this->maxScore;
+    }
+
+    public function setSubjectScore($subjectName, $value)
+    {
+        if (array_key_exists($subjectName, $this->subjects)){
+            $value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+            $value = abs($value);
+            if ($value > 50) {
+                throw new Exception('Sorry the maximum score for this subject is 50');
+            } else {
+                $this->subjects[$subjectName] = $value;
+            }
+        } else{
+            throw new Exception('Sorry we don\'nt have a subject of name '. $subjectName);
+        }
+    }
+
+    public function getSubjectScore($subjectName)
+    {
+        if (array_key_exists($subjectName, $this->subjects)){
+            $this->subjects[$subjectName] = $value;
+        } else{
+            throw new Exception('Sorry we don\'nt have a subject of name '. $subjectName);
+        }
+    }
+
+
+}
+
+class Grade1Student extends Student
+{
+    private $activity;
+    public function __construct($name, $age)
+    {
+        parent::__construct($name, $age);
+        $this->minScore = 180;
+        $this->maxScore = 330;
+        $this->subjects['Computer'] = 0;
+    }
+    public function setStudentActive()
+    {
+        $this->activity = 'Active';
+    }
+    public function setStudentAsNonActive()
+    {
+        $this->activity = 'Not Active';
+    }
+}
+
+$aya = new Grade1Student('Aya Hosny', 24);
+$aya->setSubjectScore('Arabic', 50);
+//var_dump($aya);
+echo $aya->getAge();
+echo $aya->getSubjectScore('Arabic');
+//echo $aya->getMinScore();
+//echo $aya->getMaxScore();
+print_r($aya->getSubject());
